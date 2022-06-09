8f4189d30aee6247eb8d872242ebb552e1fb452a
MikeyBadi <mihaibadia.1999@gmail.com>
2022-06-10 00:59:17 +0200

    bonus made (no style)

--- milestone2/api.php
+++ milestone2/api.php
@@ -2,7 +2,20 @@
 
   include __DIR__ . '../../disc_db.php'; 
 
+  $filteredSong=[];
+  $request= $_GET['genChosen'];
+
+  if(empty($request)){
+    $filteredSong = $discs_db;
+  }else{
+    foreach($discs_db as $disc){
+      if($disc['genre']=== $request){
+        $filteredSong[] = $disc;
+      }
+    }
+  }
+
   header('Content-Type: application/json');
 
-  echo json_encode($discs_db);
+  echo json_encode($filteredSong);
 ?>
--- milestone2/script.js
+++ milestone2/script.js
@@ -2,21 +2,29 @@ const app = new Vue({
   el:'#app',
 
   data:{
-    apiUrl:'http://localhost:8888/php-ajax-dischi/milestone2/api.php',
+    apiUrl:'http://localhost:8888/php-ajax-dischi/milestone2/api.php?genChosen=',
     discs:[],
+    genres:[],
+    genChosen:''
   },
 
   mounted() {
     this.getApi()
   },
-
+  
   methods: {
     getApi(){
-      axios.get(this.apiUrl)
+      axios.get(this.apiUrl + this.genChosen)
       .then(r => {
         this.discs = r.data
         console.log(r.data);
+        this.discs.forEach(disc => {
+          if(!this.genres.includes(disc.genre)){
+            this.genres.push(disc.genre);
+            console.log(this.genres);
+          }
+        });
       })
-    }
+    },
   },
 })
